b80c0c1eeed2730671fe810797ad057e72c32c43
Makis Tracend <makis.tracend@gmail.com>
2011-06-23 17:29:33 +1000

    Connecting with TwitterNode for basic scraping

--- server.js
+++ server.js
@@ -2,9 +2,11 @@
  * Module dependencies.
  */
 
-var express = require('express');
-
-var app = module.exports = express.createServer();
+var express = require('express'), 
+	app = module.exports = express.createServer(), 
+	config = require('./config/app.js'), 
+	TwitterNode = require('twitter-node').TwitterNode;
+	//aws = require('node-aws');
 
 // Configuration
 
@@ -15,6 +17,7 @@ app.configure(function(){
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
+  app.use(express.logger());
 });
 
 app.configure('development', function(){
@@ -40,9 +43,93 @@ app.get('/about', function(req, res){
 });
 
 
-// Only listen on $ node app.js
+// Twitter Node
+// you can pass args to create() or set them on the TwitterNode instance
+var twit = new TwitterNode({
+  user: config.twitter_user, 
+  password: config.twitter_pass,
+  //track: ["I'm happy", "I feel happy", "it's sad", "it's really sad"],
+  track: ["happy", "sad"],
+  locations: [-180, -90, 180, 90]
+});
 
+// http://apiwiki.twitter.com/Streaming-API-Documentation#QueryParameters
+//twit.params['count'] = 100;
+
+twit.headers['User-Agent'] = 'emotiglobe.com';
+
+// Make sure you listen for errors, otherwise
+// they are thrown
+twit.addListener('error', function(error) {
+  console.log(error.message);
+});
+
+twit
+  .addListener('tweet', function(tweet) {
+	var str = tweet.text;
+	if(tweet.coordinates.coordinates[0] != null){
+		var lat = Math.round(tweet.coordinates.coordinates[0]);
+	}
+	if(tweet.coordinates.coordinates[1] != null){
+		var lng = Math.round(tweet.coordinates.coordinates[1]);
+	}
+	if( str.search("happy") && lat != null && lng != null ) { 
+		console.log("Happy: " + lat +", "+ lng );
+	}
+	if( str.search("sad") && lat != null && lng != null ) { 
+		console.log("Sad: " + lat +", "+ lng );
+	}
+	//console.log("Tweet: ", tweet.text );
+	//console.log("Tweet: ", tweet.coordinates );
+  })
+
+  .addListener('limit', function(limit) {
+	console.log("LIMIT: " + sys.inspect(limit));
+  })
+
+  .addListener('delete', function(del) {
+	console.log("DELETE: " + sys.inspect(del));
+  })
+
+  .addListener('end', function(resp) {
+	console.log("wave goodbye... " + resp.statusCode);
+  })
+
+  .stream();
+
+
+// This will reset the stream
+twit.stream();
+	
+
+// AWS 
+/*
+var client = aws.createClient({
+  accessKeyId: '...',
+  secretAccessKey: '...',
+});
+
+aws.request('simpleDb', 'putAttributes', {
+  domainName: "test",
+  itemName: "item1",
+  attributes: [
+    {
+      name: 'key1',
+      value: 'val1',
+    },
+  ],
+}, function(response) {
+  if (response instanceof Error) {
+    // uh oh
+    console.log(response.code, response.message);
+  } else {
+    // it worked!
+  }
+}); 
+*/
+
+// Only listen on $ node app.js
 if (!module.parent) {
-  app.listen(3000);
+  app.listen(config.port);
   console.log("Express server listening on port %d", app.address().port);
 }
