ec1867d2322e44215dd167777630ac6e424ff8fd
Makis Tracend <makis.tracend@gmail.com>
2012-07-15 07:01:17 -0700

    Changing config options for twitter

--- config/app.sample.js
+++ config/app.sample.js
@@ -3,9 +3,13 @@ module.exports = config = {
 
   "port" : 10000,
 
-  "twitter_user" : "{USERNAME}",
-  "twitter_pass" : "{PASSWORD}",
-
+  "twitter" : {
+	  "key" : "",
+	  "secret" : "",
+	  "token_key" : "",
+	  "token_secret" : ""
+  }, 
+  
   "aws_key" : "00000000000",
   "aws_secret" : "00000000000000000",
   "aws_bucket" : "s3bucketname",
