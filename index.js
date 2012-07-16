9002bf787bc7d1e9f5237789056c3833da9d24f9
Makis Tracend <makis.tracend@gmail.com>
2012-07-15 19:27:53 -0700

    Fixing views after latest Jade update

--- index.js
+++ index.js
@@ -20,12 +20,13 @@ var express = require('express'),
 // Configuration
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+	app
+	.set('views', __dirname + '/views')
+	.set('view engine', 'jade')
+	.use(express.static(__dirname + '/public'));
 //  app.use(express.bodyParser());
 //  app.use(express.methodOverride());
 //  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
 //  app.use(express.logger());
 });
 
