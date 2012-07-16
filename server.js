fff1b16d174cb5131934a3e655111e9a749e1e63
Makis Tracend <makis.tracend@gmail.com>
2012-07-15 20:34:04 -0700

    Including configuration

--- server.js
+++ server.js
@@ -2,10 +2,10 @@
  * Module dependencies.
  */
 
-var app = require(__dirname + '/index').app;
-
+var app = require(__dirname + '/index').app, 
+	config = require(__dirname + '/config/app.js');
+	
 // Only listen on $ node app.js
 if (!module.parent) {
   app.listen(config.port);
-  //console.log("Express server listening on port %d", app.address().port);
 }
